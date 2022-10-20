3df4aa45146d862da95e0c5e822defffb8335285
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-10-20 17:48:29 -0300

    fix: add buguer menu to forum page

--- mobile/src/screens/Forum/index.tsx
+++ mobile/src/screens/Forum/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import WebView from 'react-native-webview';
+import { BuguerMenu } from '../../components/BuguerMenu';
 
 export function Forum() {
-  return (    
-    <WebView 
-      style={{ flex: 1 }}
-      source={{ uri: "http://allforone.epizy.com/"}}
-    />    
+  return (  
+    <>
+      <BuguerMenu />
+      <WebView 
+        style={{ flex: 1 }}
+        source={{ uri: "http://allforone.epizy.com/"}}
+      />    
+    </> 
   );
 }
