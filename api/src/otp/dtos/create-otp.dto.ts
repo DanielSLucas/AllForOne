e1ce6fadfb05bc942141bc44f7dd61645f142c86
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-10-20 15:04:16 -0300

    refeat: adding validations

--- api/src/otp/dtos/create-otp.dto.ts
+++ api/src/otp/dtos/create-otp.dto.ts
@@ -1,3 +1,12 @@
-export interface CreateOtpDTO {
+import { IsNotEmpty, Matches } from 'class-validator';
+
+import { MessagesHelper } from 'src/helpers/messages.helper';
+import { RegexHelper } from 'src/helpers/regex.helper';
+
+export class CreateOtpDTO {
+  @IsNotEmpty()
+  @Matches(RegexHelper.cellphone, {
+    message: MessagesHelper.valid_cellphone,
+  })
   cellphone: string;
 }
