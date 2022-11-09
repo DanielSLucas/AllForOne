adc3a6ecda469b302ea16f1897550ef7c1477ba7
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-11-09 16:42:02 -0300

    tests: refactoring and adding tests

---
+++ api/src/helpers/fakeModel.helper.ts
@@ -0,0 +1,61 @@
+import { randomUUID } from 'crypto';
+
+export class FakeModel<T = any> {
+  private entities: (T & { _id: string })[] = [];
+
+  create(data: T & { _id?: string }) {
+    const newEntity = {
+      ...data,
+      _id: data._id || randomUUID(),
+    };
+
+    this.entities.push(newEntity);
+
+    return newEntity;
+  }
+
+  updateOne(findOptions: Partial<T>, data: Partial<T>) {
+    const entityIndex = this.entities.findIndex((entity) => {
+      return Object.keys(findOptions).reduce((acc, cur) => {
+        return acc && findOptions[cur] === entity[cur];
+      }, true);
+    });
+
+    this.entities[entityIndex] = {
+      ...this.entities[entityIndex],
+      ...data,
+    };
+
+    return this.entities[entityIndex];
+  }
+
+  deleteOne(findOptions: Partial<T>) {
+    const entityIndex = this.entities.findIndex((entity) => {
+      return Object.keys(findOptions).reduce((acc, cur) => {
+        return acc && findOptions[cur] === entity[cur];
+      }, true);
+    });
+
+    this.entities.splice(entityIndex, 1);
+  }
+
+  find(findOptions?: Partial<T>) {
+    if (!findOptions || Object.keys(findOptions).length === 0) {
+      return this.entities;
+    }
+
+    return this.entities.filter((entity) => {
+      return Object.keys(findOptions).reduce((acc, cur) => {
+        return acc && findOptions[cur] === entity[cur];
+      }, true);
+    });
+  }
+
+  findOne(findOptions: Partial<T>) {
+    return this.entities.find((entity) => {
+      return Object.keys(findOptions).reduce((acc, cur) => {
+        return acc && findOptions[cur] === entity[cur];
+      }, true);
+    });
+  }
+}
--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -1,10 +1,15 @@
 import { Model } from 'mongoose';
-import { HttpException } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  NotFoundException,
+} from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { User, UserDocument } from './schemas/user.schema';
 import { UsersService } from './users.service';
+import { FakeModel } from '../helpers/fakeModel.helper';
 
 const users: UserDocument[] = [
   {
@@ -31,10 +36,7 @@ describe('UsersService', () => {
         UsersService,
         {
           provide: getModelToken(User.name),
-          useValue: {
-            create: jest.fn(),
-            find: jest.fn().mockResolvedValue(users),
-          },
+          useValue: new FakeModel<User>(),
         },
       ],
     }).compile();
@@ -50,15 +52,10 @@ describe('UsersService', () => {
 
   describe('create', () => {
     it('should create a new user with name, cellphone and eula.', async () => {
-      jest.spyOn(usersModel, 'create').mockResolvedValueOnce(users[0] as never);
-
-      const result = await usersService.create({
-        name: 'User 1',
-        cellphone: '12912344321',
-        eula: true,
-      });
+      const result = await usersService.create(users[0]);
 
       expect(result).toEqual(users[0]);
+      expect(result).toHaveProperty('_id');
       expect(result).toHaveProperty('name');
       expect(result).toHaveProperty('cellphone');
       expect(result).toHaveProperty('eula');
@@ -76,13 +73,81 @@ describe('UsersService', () => {
       ).rejects.toBeInstanceOf(HttpException);
       expect(createFunc).not.toHaveBeenCalled();
     });
+
+    it('should not be able to create two users with the same cellphone number', async () => {
+      await usersService.create(users[0]);
+      await expect(usersService.create(users[0])).rejects.toBeInstanceOf(
+        BadRequestException,
+      );
+    });
+  });
+
+  describe('update', () => {
+    it("should be able to update user's info", async () => {
+      await usersService.create(users[0]);
+
+      const result = await usersService.update(users[0]._id, {
+        name: users[0].name,
+        cellphone: '12912344322',
+      });
+
+      expect(result.cellphone).toEqual('12912344322');
+    });
+  });
+
+  describe('delete', () => {
+    it('should be able to delete an user', async () => {
+      await usersService.create(users[0]);
+
+      const usersBeforeDeletion = await usersService.findAll();
+      expect(usersBeforeDeletion.length).toBe(1);
+
+      await expect(usersService.delete(users[0]._id)).resolves.not.toThrow();
+
+      const usersAfterDeletion = await usersService.findAll();
+      expect(usersAfterDeletion.length).toBe(0);
+    });
   });
 
   describe('findAll', () => {
     it('should return a list of users.', async () => {
+      await Promise.all(users.map((user) => usersService.create(user)));
+
       const result = await usersService.findAll();
 
       expect(result).toEqual(users);
+    });
+  });
+
+  describe('findById', () => {
+    it('should be able to find an user by his id', async () => {
+      await Promise.all(users.map((user) => usersService.create(user)));
+
+      const user = await usersService.findById(users[0]._id);
+
+      expect(user).toEqual(users[0]);
+    });
+
+    it('should not be able to find an non-existent user', async () => {
+      await expect(
+        usersService.findById('non-existent user id'),
+      ).rejects.toBeInstanceOf(NotFoundException);
+    });
+  });
+
+  describe('findByCellphone', () => {
+    it('should be able to find an user by his cellphone number', async () => {
+      await Promise.all(users.map((user) => usersService.create(user)));
+
+      const user = await usersService.findByCellphone(users[0].cellphone);
+
+      expect(user).toEqual(users[0]);
+    });
+
+    it('should not be able to find an non-existent user', async () => {
+      await expect(
+        usersService.findByCellphone('non-existent user cellphone'),
+      ).rejects.toBeInstanceOf(NotFoundException);
     });
   });
 });
